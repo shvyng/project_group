71a14c50b4897c96bd372175f68db298775c940a
Rachael-Lim-Xin-Yi <S10262632@connect.np.edu.sg>
2023-08-02 22:03:12 +0800

    Update: First part

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,1 +1,11 @@
-#cash on hand assigned to rachael lim
+def coh_surplus(csv_data):
+    try: #Double check required if the code could be used
+        prev_day_coh = float(csv_data[0][-1])
+        for row in csv_data[1:]:
+            if float(row[-1]) < prev_day_coh:
+                return False
+            else:
+                prev_day_coh = float(row[-1])
+        return True
+    except (ValueError, IndexError):#Double check required if the code could be used
+        return False#Double check required if the code could be used
