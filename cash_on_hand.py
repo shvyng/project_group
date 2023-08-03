a6bba0dbf62834ec00aeecccf626dafffd6ab288
Rachael-Lim-Xin-Yi <S10262632@connect.np.edu.sg>
2023-08-03 11:37:42 +0800

    2nd Part: Please help me to check thanks!

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -9,3 +9,21 @@ def coh_surplus(csv_data):
         return True
     except (ValueError, IndexError):#Double check required if the code could be used
         return False#Double check required if the code could be used
+    
+def process_coh_data(data):
+    coh_data = []  # List to store day number and COH difference pairs
+    prev_day_coh = None  # Variable to store previous day's COH
+    day_no = 1  # Day number starts from 1
+    
+    for row in data:
+        if prev_day_coh is not None and prev_day_coh > float(row[-1]):#Double check required if the code could be used
+            coh_difference = prev_day_coh - float(row[-1])#Double check required if the code could be used
+        prev_day_coh = float(row[-1])
+        day_no += 1
+    return coh_data
+
+highest_surplus = 0
+prev_day_coh = float(csv_data[0][-1])
+highest_day = 0
+for day_no, row in enumerate(csv_data, start=1):#Double check required if the code could be used
+# The variable `day_no` can now be used outside the loop if needed.
