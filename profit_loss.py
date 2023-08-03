110e2ecbf594df0ab5df461176ec014b6345a015
peiiyyiingg <peiiyyiingg0711@gmail.com>
2023-08-04 00:52:18 +0800

    edited joc part

--- profit_loss.py
+++ profit_loss.py
@@ -1,7 +1,7 @@
 # Contains functions used for calculating profit and loss
 
 def check_net_profit_surplus(csv_data):
-    prev_day_profit = float(csv_data.replace(',',''))
+    prev_day_profit = float(csv_data[0][-1].replace(',',''))
     for row in csv_data:
         row [-1]=row[-1].replace (',','')
         if float (row[-1]) < prev_day_profit:
@@ -13,8 +13,8 @@ def check_net_profit_surplus(csv_data):
 def calc_profit_loss(csv_data,surpluscheck):
     profit_data = []
     if surpluscheck == False:
-        prev_day_profit = float(csv_data)
+        prev_day_profit = float(csv_data[0][-1])
         day_no = 1
         for row in csv_data:
             row [-1] = row[-1].replace (',','')
-            if prev_day_profit < float(row[-1]):
+            if prev_day_profit > float(row[-1]):
