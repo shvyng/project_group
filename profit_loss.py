bd6f85b9b91ac4f566b17d34fc4998db67978031
peiiyyiingg <peiiyyiingg0711@gmail.com>
2023-08-04 08:36:24 +0800

    update: part 2 of the code please help check

--- profit_loss.py
+++ profit_loss.py
@@ -18,3 +18,21 @@ def calc_profit_loss(csv_data,surpluscheck):
         for row in csv_data:
             row [-1] = row[-1].replace (',','')
             if prev_day_profit > float(row[-1]):
+                profit_data.append([day_no, (prev_day_profit - float(row[-1]))])
+            prev_day_profit = float(row[-1])
+            day_no += 1
+        return profit_data
+    else:
+        hightest_surplus = 0 
+        prev_day_profit = float(csv_data[0][-1])
+        highest_day = 0 
+        day_no = 1
+        for now in csv_data:
+            row[-1] = float(row[-1], replace(",",""))
+            current_surplus = row[-1] - prev_day_profit
+            if current_surplus < hightest_surplus:
+                hightest_surplus = current_surplus
+                highest_day = day_no
+            day_no += 1
+            prev_day_profit = row[-1]
+        return(([highest_day,hightest_surplus]))
