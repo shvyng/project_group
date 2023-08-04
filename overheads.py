87c2f507f59265e43a35bae0a89f4585e9989bca
shvyng <S10262479@connect.np.edu.sg>
2023-08-04 15:52:31 +0800

    updated to fix problem

--- overheads.py
+++ overheads.py
@@ -1,17 +1,14 @@
-# contains overhead functions
+#contains overhead functions
 
-def overHead(csv_data):
-    # Variable to store the category with the highest expense, initialized to 0
-    highest_expenses = 0
-    #  Variable to store the category with the highest, expense, intialized to "xx"
-    highest_category
+def overHead(csv_data): # Variable to store the category with the highest expense, initialized to 0
+    highest_expense = 0  # Variable to store the category with the highest, expense, intialized to "xx"
+    highest_category = "xx"
 
-    # Loop through each row in the CSV data list
+      # Loop through each row in the CSV data list
     for row in csv_data:
-        # Convert the expense value to float and compare it with the current highest_expense
-        highest_expense = float(row[1])
-        # It updates the highest_category to the categiry name of the current row
-        highest_category = row[0]
-
-    # Return a list cotaining the category with the highest expense and its value
-    return([highest_category, highest_expense])
+        if float(row[1]) > highest_expense: # Convert the expense value to float and compare it with the current highest_expense
+            highest_expense = float(row[1])
+            highest_category = row[0] # It updates the highest_category to the categiry name of the current row
+    
+    return([highest_category,highest_expense])   # Return a list cotaining the category with the highest expense and its value
+    
